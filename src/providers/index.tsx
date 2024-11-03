c36566fd52651017325be0bf714c7c3e52944736
Mohammed Haydar <mr.hamood277@gmail.com>
2024-11-03 11:55:03 +0000

    Refactor theme provider in Providers component

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,14 +1,20 @@
+import { ThemeProvider } from 'next-themes'
 import NextTopLoader from 'nextjs-toploader'
 import { Toaster } from '@/components/ui/sonner'
 import AuthProvider from '@/providers/auth-provider'
-import { ThemeProvider } from '@/providers/theme-provider'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
 
-async function Providers({ children }: { children: React.ReactNode }) {
+async function Providers({
+  children,
+  ...props
+}: { children: React.ReactNode } & ThemeProviderProps) {
   return (
     <>
       <NextTopLoader color='#007AFF' showAtBottom={false} zIndex={1600} />
       <AuthProvider>
-        <ThemeProvider attribute='class'>{children}</ThemeProvider>
+        <ThemeProvider attribute='class' {...props}>
+          {children}
+        </ThemeProvider>
       </AuthProvider>
       <Toaster />
     </>
