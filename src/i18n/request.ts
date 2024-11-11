6678a884a6098383b542abf0a7215c522aaf3d5e
Mohammed Haydar <mr.hamood277@gmail.com>
2024-11-11 12:39:41 +0000

    Refactor: Update language handling in i18n request module

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,7 @@ import { getLocale, getRequestConfig } from 'next-intl/server'
 import { getUserLanguage } from '@/actions/users/user-language'
 
 export default getRequestConfig(async () => {
-  let locale = (await getUserLanguage()) ?? (await getLocale())
+  const locale = (await getUserLanguage()) ?? (await getLocale())
 
   return { locale, messages: (await import(`../../messages/${locale}.json`)).default }
 })
